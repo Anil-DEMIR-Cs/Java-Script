4f471ada9c8c7a42df455e45955fe1d50f9f33cc
anil-demir-cs <anildemir4392cs@gmail.com>
2024-02-02 17:00:12 +0300

    added chapter 11

---
+++ 11-if-else-3.js
@@ -0,0 +1,14 @@
+let mezuniyet = "üniversite";
+let yas = "20";
+
+if (yas >= 18 && (mezuniyet == "lise" || mezuniyet == "üniversite"))
+{
+    console.log("ehliyet alabilir");
+}
+else
+{
+    console.log("ehliyet şartları tutmuyor");
+}
+
+// && and demek 
+// || or demek
