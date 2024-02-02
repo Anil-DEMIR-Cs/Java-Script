f17fbf982ed1db649bf7f63dc40fd7d730103479
anil-demir-cs <anildemir4392cs@gmail.com>
2024-02-02 16:47:29 +0300

    add chapter 9

---
+++ 9-if-else-1.js
@@ -0,0 +1,12 @@
+if (3 < 4) {
+    console.log("3 küçüktür 4 ten");
+}
+
+
+let isLoggedin = true;
+if (isLoggedin) {
+    console.log("uygulamaya giriş yapıldı.");
+}
+else {
+    console.log("başarısız.");
+}
