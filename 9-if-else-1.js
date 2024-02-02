370fd30f31e0a819fa48465db717167046ca04a7
anil-demir-cs <anildemir4392cs@gmail.com>
2024-02-02 16:54:36 +0300

    revise

--- 9-if-else-1.js
+++ 9-if-else-1.js
@@ -1,12 +1,15 @@
-if (3 < 4) {
+if (3 < 4) 
+{
     console.log("3 küçüktür 4 ten");
 }
 
 
 let isLoggedin = true;
-if (isLoggedin) {
+if (isLoggedin)
+{
     console.log("uygulamaya giriş yapıldı.");
 }
-else {
+else
+{
     console.log("başarısız.");
 }
