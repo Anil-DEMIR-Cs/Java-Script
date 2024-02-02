29d5723eb2c4359ddf66b58e8428ca692f14e4c1
anil-demir-cs <anildemir4392cs@gmail.com>
2024-02-02 16:52:33 +0300

    added chapter 10

---
+++ 10-if-else-2.js
@@ -0,0 +1,18 @@
+let username = "anildemir";
+let password = "1234";
+
+if (username == "anildemir") 
+{
+    if (password == "1234") 
+    {
+        console.log("uygulamaya giriş yapıldı.");
+    }
+    else
+    {
+        console.log("parola yanlış");
+    }
+}
+else
+{
+    console.log("kullanici adi yanlış.");
+}
