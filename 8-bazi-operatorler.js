fc3c6affbb468ef081dfc11d108cf829bc4ee6cd
anil-demir-cs <anildemir4392cs@gmail.com>
2024-02-02 16:39:47 +0300

    changed

---
+++ 8-bazi-operatorler.js
@@ -0,0 +1,17 @@
+var a = 5, b= 2;
+
+var sonuc = a % b;
+
+// % bölme işleminin kalanını verir.
+
+console.log(sonuc);
+
+// a == b   a b ye eşit mi biri number biri string de olsa eşit der bu işlem yapacaksak alttakini kullanmak lazım.
+// a === b  a b ye gerçekten eşitmi ikisininde number veya ikisininde string olmasını ister.
+// a != b   a b ye eşit değil mi
+// a > b   a b den büyük mü
+// a < b   a b den küçük mü
+// a <= b  a b ye küçük eşit mi
+
+
+
